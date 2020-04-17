2f542048eb651eea959075ae92dbd00d1bbcdd3f
bestshubhamever <33572182+bestshubhamever@users.noreply.github.com>
2020-04-17 21:32:08 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/dbhelper.dart';
+import 'package:flutter_app/studentmodel.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: StudentPage(),
+    );
+  }
+}
+
+class StudentPage extends StatefulWidget {
+  @override
+  _StudentPageState createState() => _StudentPageState();
+}
+
+class _StudentPageState extends State<StudentPage> {
+  final GlobalKey<FormState> _formStateKey = GlobalKey<FormState>();
+  Future<List<Student>> students;
+  String _studentName;
+  bool isUpdate = false;
+  int studentIdForUpdate;
+  DBHelper dbHelper;
+  final _studentNameController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    dbHelper = DBHelper();
+    refreshStudentList();
+  }
+
+  refreshStudentList() {
+    setState(() {
+      students = dbHelper.getStudents();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('demo app'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Form(
+            key: _formStateKey,
+            autovalidate: true,
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
+                  child: TextFormField(
+                    onSaved: (value) {
+                      _studentName = value;
+                    },
+                    controller: _studentNameController,
+                    decoration: InputDecoration(
+                      labelText: "Student Name",
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                color: Colors.purple,
+                child: Text(
+                  (isUpdate ? 'UPDATE' : 'ADD'),
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () {
+                  if (isUpdate) {
+                    if (_formStateKey.currentState.validate()) {
+                      _formStateKey.currentState.save();
+                      dbHelper
+                          .update(Student(studentIdForUpdate, _studentName))
+                          .then((data) {
+                        setState(() {
+                          isUpdate = false;
+                        });
+                      });
+                    }
+                  } else {
+                    if (_formStateKey.currentState.validate()) {
+                      _formStateKey.currentState.save();
+                      dbHelper.add(Student(null, _studentName));
+                    }
+                  }
+                  _studentNameController.text = '';
+                  refreshStudentList();
+                },
+              ),
+              Padding(
+                padding: EdgeInsets.all(10),
+              ),
+            ],
+          ),
+          const Divider(
+            height: 5.0,
+          ),
+          Expanded(
+            child: FutureBuilder(
+              future: students,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return generateList(snapshot.data);
+                }
+                if (snapshot.data == null || snapshot.data.length == 0) {
+                  return Text('No Data Found');
+                }
+                return CircularProgressIndicator();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  SingleChildScrollView generateList(List<Student> students) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: DataTable(
+          columns: [
+            DataColumn(
+              label: Text('NAME'),
+            ),
+          ],
+          rows: students
+              .map(
+                (student) => DataRow(
+                  cells: [
+                    DataCell(
+                      Text(student.name),
+                      onTap: () {
+                        setState(() {
+                          isUpdate = true;
+                          studentIdForUpdate = student.id;
+                        });
+                        _studentNameController.text = student.name;
+                      },
+                    ),
+                  ],
+                ),
+              )
+              .toList(),
+        ),
+      ),
+    );
+  }
+}
